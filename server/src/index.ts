2519e5f914337c4921837bbee275c52f908121d4
Ngo Gia Phong <ngogiaphong123@gmail.com>
2022-10-02 16:00:02 +0700

    Error Handler

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,7 +1,9 @@
 import dotenv from 'dotenv'
 dotenv.config();
-import express, { Response,Request } from 'express';
+import express, { Response,Request, NextFunction } from 'express';
 import logger from './utils/logger'
+import ExpressError from './utils/expressError';
+import { StatusCodes } from 'http-status-codes';
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
@@ -13,6 +15,13 @@ app.get('/healthcheck', (req : Request, res : Response) => {
 app.post('/api/users/register', (req : Request , res : Response) => {
 
     res.send(req.body)
+})
+app.all('*', (req: Request, res: Response, next: NextFunction) => {
+    return next(new ExpressError('Not Found', StatusCodes.NOT_FOUND))
+})
+app.use((err: ExpressError, req: Request, res: Response, next: NextFunction) => {
+    const { status = 500, message = 'Something went wrong' } = err;
+    res.status(status).send(message)
 })
 app.listen(PORT , () => {
     logger.info(`Server is running on port ${PORT}`);
